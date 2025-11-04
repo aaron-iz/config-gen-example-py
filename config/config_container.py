a483f38e73991af541b5de73131c6e480b84d843
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-11-04 22:53:33 +0000

    add env to the loader/container

--- config/config_container.py
+++ config/config_container.py
@@ -1,5 +1,8 @@
 from config.models.app_config import AppConfig
 from config.models.auth_routes_config import AuthRoutesConfig
+import os
+
+ENV_NAME_ENV_VAR_KEY = "SERVICE_ENV_NAME"
 
 class ConfigContainer:
     """
@@ -7,8 +10,17 @@ class ConfigContainer:
     """
     AppConfig: AppConfig
     AuthRoutesConfig: AuthRoutesConfig
+    
+    def __init__(self):
+        self._env = os.environ.get(ENV_NAME_ENV_VAR_KEY, None)
+        
+    @property
+    def env(self):
+        return self._env
 
 # Load all configs automatically
 config = ConfigContainer()
-config.AppConfig = AppConfig.load()
-config.AuthRoutesConfig = AuthRoutesConfig.load()
+
+# register your config here and pass it the env:
+config.AppConfig = AppConfig.load(config.env)
+config.AuthRoutesConfig = AuthRoutesConfig.load(config.env)
