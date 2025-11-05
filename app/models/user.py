d8b6d9a6610fb353aec72ecaeeddb2bca22fd961
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-11-05 22:45:00 +0000

    expand user

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,14 @@
+import datetime
 from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
 
 USERNAME_MIN_LENGTH = 3
 PASSWORD_MIN_LENGTH = 8
 
 class User(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
     username: str = Field(..., min_length=USERNAME_MIN_LENGTH)
     password: str = Field(..., min_length=PASSWORD_MIN_LENGTH)
+    full_name: str = Field(..., min_length=1)
+    time_created: datetime = Field(default_factory=datetime.utcnow)
+    last_password_changed: datetime = Field(default_factory=datetime.utcnow)
