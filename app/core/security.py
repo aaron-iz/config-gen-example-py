b25f9bb4f3de32e45d29a0a81ba9adf0bc9ce4b4
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-11-05 22:59:00 +0000

    simple jwt class

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,47 @@
 import jwt
+from config.config_container import config
+
 
 # Consts:
 ALGO = "HS256"
 
+
 # Properties:
 __jwt_secret = None
+
+
+# Load jwt secret
+def load_jwt_secret():
+    global __jwt_secret
+    filename = config.AppConfig.JwtSecretFileName
+    
+    with open(filename, "r") as f:
+        __jwt_secret = f.read()
+    
+    __jwt_secret
+
+
+def secret():
+    if __jwt_secret is None:
+        load_jwt_secret()
+        
+    if __jwt_secret is None:
+        raise Exception("Failed to load JWT secret")
+    
+    return __jwt_secret
+
+
+# JWT functions
+def create_token(data: dict):
+    return jwt.encode(data, secret(), algorithm=ALGO)
+
+
+def decode_token(token: str):
+    return jwt.decode(token, secret(), algorithms=[ALGO])
+
+
+def verify_token(token: str):
+    try:
+        return decode_token(token)
+    except:
+        return None
